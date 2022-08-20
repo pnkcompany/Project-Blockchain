1f1fbea375dc33b443e3473a9bd24c92cece294b
Priyanka Muchandikar <priyamuchandikar@gmail.com>
2022-08-19 22:49:14 -0700

    changed config

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -58901,7 +58901,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "5465" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "10131" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
